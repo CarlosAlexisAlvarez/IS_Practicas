65b03428326fe8e7a3626909e3e341184afaa189
CarlosAlexisAlvarez <47435890+CarlosAlexisAlvarez@users.noreply.github.com>
2019-08-30 22:22:27 -0500

    Blog Personal

--- PersonalBlog/Home/views.py
+++ PersonalBlog/Home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+	{
+		'author': 'Carlos Alexis',
+		'title': 'Mi Blog Personal',
+		'content': 'Mi nombre es Carlos pero me conocen como Zerafin, se bienvenido',	
+		'content': 'Me gustan los Videojuegos y leer Manga',
+		'date_posted': '30 de Agosto del 2019',	
+	}
+]
+
 
 def index(request):
-    return HttpResponse("Hola")
+	context = {
+		'posts': posts
+		}
+    return render(request, 'Home/templates.html', context)
